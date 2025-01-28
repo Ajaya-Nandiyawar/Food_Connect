f82251349c20ae7dc9d59630348d3b86af2d531f
Ajaya-Nandiyawar <ajaynandiyawar5621@gmail.com>
2025-01-28 21:24:13 +0530

    dashboard files

---
+++ dashboard/setting.js
@@ -0,0 +1,7 @@
+function messageUs() {
+    alert('Message functionality will be implemented here');
+}
+
+function scheduleCall() {
+    alert('Call scheduling functionality will be implemented here');
+}
