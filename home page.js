f41cde266c93ba3a948100bc15326e91f9146e01
Manthan80877 <agrawalmanthan9975@gmail.com>
2025-02-24 19:54:00 +0530

    go to top button

--- home page.js
+++ home page.js
@@ -5,3 +5,18 @@
           navBar=document.querySelector(".nav-bar");
           navBar.classList.toggle("active");
       }
+
+      window.onscroll = function() {
+        var goToTop = document.getElementById("goToTop");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          goToTop.style.display = "block";
+        } else {
+          goToTop.style.display = "none";
+        }
+      };
+      
+      function scrollToTop(event) {
+        event.preventDefault();
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
