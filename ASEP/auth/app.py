794aef8a85a2dcd1d064aad0dc87e9293d5950d6
Ajaya <ajaynandiyawar5621@gmail.com>
2025-02-27 14:54:16 +0530

    checkbox

--- ASEP/auth/app.py
+++ ASEP/auth/app.py
@@ -34,9 +34,9 @@ s = URLSafeTimedSerializer(app.secret_key)
 
 # MySQL connection string
 db_user = "root"
-db_password = "%40J%21nky%40ub%40le5"  # URL-encoded password (%40 represents @)
+db_password = "Ajaya%405621"  # URL-encoded password (%40 represents @)
 db_host = "127.0.0.1"
-db_name = "registered"
+db_name = "FOODCONNECT"
 
 # Configuring database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}'
