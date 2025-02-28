577434db2dc9158744eb37b3aacefa738602bc1d
Manthan80877 <agrawalmanthan9975@gmail.com>
2025-02-28 18:35:02 +0530

    slider and mission in home page

--- ASEP/auth/app.py
+++ ASEP/auth/app.py
@@ -48,9 +48,9 @@ s = URLSafeTimedSerializer(app.secret_key)
 
 # MySQL connection string
 db_user = "root"
-db_password = "%40J%21nky%40ub%40le5"  # URL-encoded password (%40 represents @)
+db_password = "Manthan%409975"  # URL-encoded password (%40 represents @)
 db_host = "127.0.0.1"
-db_name = "registered"
+db_name = "Food_Connect"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
