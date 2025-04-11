dff4faf21feb45fdbb0bde0ad537cc0469a585a0
rishiagrawal02 <rishisagrawal02@gmail.com>
2025-04-12 00:45:22 +0530

    Database Migrated!!

--- ASEP/auth/app.py
+++ ASEP/auth/app.py
@@ -61,13 +61,15 @@ app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
 mail.init_app(app)  # Initialize mail with app
 s = URLSafeTimedSerializer(app.secret_key)
 
-# MySQL configuration
+# PostgreSQL configuration - UPDATED FROM MySQL
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
 db_name = os.getenv('DB_NAME')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Register blueprints
@@ -401,7 +403,6 @@ def reset_password(token):
         return redirect(url_for('login'))
 
     return render_template('reset_pass.html', form=form, token=token)
-
 
 
 # Run the app
